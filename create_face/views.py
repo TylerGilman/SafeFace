aae20c55b38a1b66a9730426c2582b548a22ecdb
Tyler Gilman <tylersgilman@gmail.com>
2024-06-13 17:08:46 -0400

    working demo, fixed cleaning up cuda recourses

--- create_face/views.py
+++ create_face/views.py
@@ -28,7 +28,7 @@ def create(request):
   pipe.to("cuda")
 
   # Define prompts and generate image
-  prompt = "Hyper-Realism, Profile View, White Background, of a person with the following attributes: " + request.POST.get('hair_color')+ ", " + request.POST.get('eye_color') + ", " + request.POST.get('skin_type') + ", " + request.POST.get('ethnicity') + ", " + request.POST.get('gender') + ", " + request.POST.get('bodyfat') + ", face directly forward."
+  prompt = "Hyper-Realism, Profile View, White Background, of a single person's face with the following attributes: " + request.POST.get('hair_color')+ ", " + request.POST.get('eye_color') + ", " + request.POST.get('skin_type') + ", " + request.POST.get('ethnicity') + ", " + request.POST.get('gender') + ", " + request.POST.get('bodyfat') + ", face directly forward."
   negative_prompt = ""
 
   image = pipe(
@@ -44,7 +44,9 @@ def create(request):
 
   image.save("static/images/generated_image.png")
   
-  # return image
+  # Clean up resources
+  del vae
+  del pipe
   torch.cuda.empty_cache()
 
   
