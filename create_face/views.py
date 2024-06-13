23fc3ad77a60fcfd7a31c00452bc44c85f201c2c
Tyler Gilman <tylersgilman@gmail.com>
2024-06-13 02:57:18 -0400

    face foward

--- create_face/views.py
+++ create_face/views.py
@@ -28,7 +28,7 @@ def create(request):
   pipe.to("cuda")
 
   # Define prompts and generate image
-  prompt = "Hyper-Realism, Profile View, White Background, of a person with the following attributes: " + request.POST.get('hair_color')+ ", " + request.POST.get('eye_color') + ", " + request.POST.get('skin_type') + ", " + request.POST.get('ethnicity') + ", " + request.POST.get('gender') + ", " + request.POST.get('bodyfat') + ", directly facing the camera."
+  prompt = "Hyper-Realism, Profile View, White Background, of a person with the following attributes: " + request.POST.get('hair_color')+ ", " + request.POST.get('eye_color') + ", " + request.POST.get('skin_type') + ", " + request.POST.get('ethnicity') + ", " + request.POST.get('gender') + ", " + request.POST.get('bodyfat') + ", face directly forward."
   negative_prompt = ""
 
   image = pipe(
