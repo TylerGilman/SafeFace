6a27d1b325303c241b27ebc1fcc8c830144da964
Silas Nevstad <nevstads@gmail.com>
2024-06-17 16:02:42 -0400

    create w hugging face

--- create_face/urls.py
+++ create_face/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("create", views.create, name="create"),
+    path("create_with_hugging_face", views.create_with_hugging_face, name="create_with_hugging_face"),
 ]
--- create_face/views.py
+++ create_face/views.py
@@ -6,32 +6,38 @@ from diffusers import (
     KDPM2AncestralDiscreteScheduler,
     AutoencoderKL,
 )
-from diffusers.models.modeling_outputs import Transformer2DModelOutput
 import time
+import requests
+import io
+from PIL import Image
 
-# Load VAE component
-vae = AutoencoderKL.from_pretrained(
-  "madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16
-)
-
-# Configure the pipeline
-pipe = StableDiffusionXLPipeline.from_pretrained(
-  "Corcelio/mobius", vae=vae, torch_dtype=torch.float16
-)
-pipe.scheduler = KDPM2AncestralDiscreteScheduler.from_config(pipe.scheduler.config)
-pipe.to("cuda")
+API_URL = "https://api-inference.huggingface.co/models/Corcelio/mobius"
+headers = {"Authorization": "Bearer REPLACE_ME"}
 
 
 def index(request):
-  return render(request, "index.html", None)
+    return render(request, "index.html", None)
+
 
 @csrf_exempt
 def create(request):
-  # Define prompts and generate image
-  prompt = "Hyper-Realism, Front Face View, White Background, of a single person's face with the following attributes: " + request.POST.get('hair_color') + " " + request.POST.get('hair_type') + " " + request.POST.get('hair_length') + " " + request.POST.get('skin_type') + " " + request.POST.get('eye_color') + " " + request.POST.get('skin_color') + " " + request.POST.get('ethnicity') + " " + request.POST.get('gender') + " " + request.POST.get('bodyfat') + " " + request.POST.get('age') + " facing directly forward."
-  negative_prompt = ""
+    # Load VAE component
+    vae = AutoencoderKL.from_pretrained(
+        "madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16
+    )
 
-  image = pipe(
+    # Configure the pipeline
+    pipe = StableDiffusionXLPipeline.from_pretrained(
+        "Corcelio/mobius", vae=vae, torch_dtype=torch.float16
+    )
+    pipe.scheduler = KDPM2AncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+    pipe.to("cuda")
+
+    # Define prompts and generate image
+    prompt = make_prompt(request)
+    negative_prompt = ""
+
+    image = pipe(
       prompt=prompt,
       negative_prompt=negative_prompt,
       width=1024,
@@ -39,12 +45,39 @@ def create(request):
       guidance_scale=7,
       num_inference_steps=50,
       clip_skip=3,
-  ).images[0]
+    ).images[0]
 
-  image.save("static/images/generated_image.png")
-  
-  torch.cuda.empty_cache()
-  
-  static_path = "/static/images/generated_image.png"
-  # add t as a parameter to prevent caching
-  return render(request, "avatar_display.html", {'image_path': static_path + "?t=" + str(time.time())})
+    image.save("static/images/generated_image.png")
+
+    torch.cuda.empty_cache()
+
+    static_path = "/static/images/generated_image.png"
+    # add t as a parameter to prevent caching
+    return render(request, "avatar_display.html", {'image_path': static_path + "?t=" + str(time.time())})
+
+
+@csrf_exempt
+def create_with_hugging_face(request):
+    prompt = make_prompt(request)
+
+    image_bytes = query({
+        "inputs": prompt,
+    })
+
+    image = Image.open(io.BytesIO(image_bytes))
+    image.save("static/images/generated_image.png")
+    static_path = "/static/images/generated_image.png"
+    return render(request, "avatar_display.html", {'image_path': static_path + "?t=" + str(time.time())})
+
+
+def make_prompt(request):
+    return (("Hyper-Realism, Front Face View, White Background, of a single person's face with the following "
+             "attributes: ") + request.POST.get('hair_color') + " " + request.POST.get('hair_type') + " " +
+            request.POST.get('hair_length') + " " + request.POST.get('skin_type') + " " + request.POST.get(
+                'eye_color') + " " + request.POST.get('skin_color') + " " + request.POST.get('ethnicity') + " " +
+            request.POST.get('gender') + " " + request.POST.get('bodyfat') + " " + request.POST.get('age') + " facing directly forward.")
+
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.content
