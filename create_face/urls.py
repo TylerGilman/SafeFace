9348e4d65666f82be5f82d5171ad0be0041954cc
Silas Nevstad <nevstads@gmail.com>
2024-06-18 19:16:15 -0400

    Not found / error fix + nit restructure

--- create_face/urls.py
+++ create_face/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("create", views.index, name="create"),
-    path("create_with_hugging_face", views.create_with_hugging_face, name="create_with_hugging_face"),
 ]
--- create_face/views.py
+++ create_face/views.py
@@ -7,15 +7,17 @@ import time
 pipeline_handler = PipelineHandler()
 hugging_face_handler = HuggingFaceHandler()
 
+
 @csrf_exempt
 def create(request):
-  if request.POST.get("generate_method") == "pipeline":
-    return create_with_pipline(request)
-  elif request.POST.get("generate_method") == "hugging_face":
-    return create_with_hugging_face(request)
-  else:
-    return render(request, "error.html", {"message": "Invalid generate method."})
-      
+    if request.POST.get("generate_method") == "pipeline":
+        return create_with_pipline(request)
+    elif request.POST.get("generate_method") == "hugging_face":
+        return create_with_hugging_face(request)
+    else:
+        return render(request, "error.html", {"message": "Invalid generate method."})
+
+
 @csrf_exempt
 def create_with_pipline(request):
     prompt = make_prompt(request)
@@ -23,9 +25,11 @@ def create_with_pipline(request):
     if image:
         image.save("static/images/generated_image.png")
         static_path = "/static/images/generated_image.png"
-        return render(request, "avatar_display.html", {"generate_method": "pipeline", "image_path": static_path + "?t=" + str(time.time())})
+        return render(request, "avatar_display.html",
+                      {"generate_method": "pipeline", "image_path": static_path + "?t=" + str(time.time())})
     else:
         return render(request, "error.html", {"message": "Failed to generate image using pipeline."})
+
 
 @csrf_exempt
 def create_with_hugging_face(request):
@@ -34,28 +38,31 @@ def create_with_hugging_face(request):
     if image:
         image.save("static/images/generated_image.png")
         static_path = "/static/images/generated_image.png"
-        return render(request, "avatar_display.html", {"generate_method": "hugging_face", "image_path": static_path + "?t=" + str(time.time())})
+        return render(request, "avatar_display.html",
+                      {"generate_method": "hugging_face", "image_path": static_path + "?t=" + str(time.time())})
     else:
         return render(request, "error.html", {"message": "Failed to generate image using Hugging Face."})
 
+
 def make_prompt(request):
     return (
-        "Hyper-Realism, Front Face View, White Background, of a single person's clean face with the following attributes: "
-        + request.POST.get("hair_color") + " "
-        + request.POST.get("hair_type") + " "
-        + request.POST.get("hair_length") + " "
-        + request.POST.get("skin_type") + " "
-        + request.POST.get("eye_color") + " "
-        + request.POST.get("skin_color") + " "
-        + request.POST.get("ethnicity") + " "
-        + request.POST.get("gender") + " "
-        + request.POST.get("body") + " "
-        + request.POST.get("age") + " facing directly forward."
+            "Hyper-Realism, Front Face View, White Background, of a single person's clean face with the following attributes: "
+            + request.POST.get("hair_color") + " "
+            + request.POST.get("hair_type") + " "
+            + request.POST.get("hair_length") + " "
+            + request.POST.get("skin_type") + " "
+            + request.POST.get("eye_color") + " "
+            + request.POST.get("skin_color") + " "
+            + request.POST.get("ethnicity") + " "
+            + request.POST.get("gender") + " "
+            + request.POST.get("body") + " "
+            + request.POST.get("age") + " facing directly forward."
     )
+
 
 @csrf_exempt
 def index(request):
-    if request.method=="POST":
+    if request.method == "POST":
         return create(request)
-    ## Just get the page
+    # Just get the page
     return render(request, "index.html", None)
