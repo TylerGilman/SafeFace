a50074384a157ea49020615b7cb21902fa7097ba
Tyler Gilman <tylersgilman@gmail.com>
2024-06-21 15:43:18 -0400

    ui updates

--- create_face/views.py
+++ create_face/views.py
@@ -22,16 +22,16 @@ def index(request):
         return create(request)
     # Just get the page
     form_fields = [
-        {'id': 'gender', 'label': 'Gender', 'placeholder': 'Select Gender', 'options': ['Male', 'Female', 'Ugly']},
-        {'id': 'hair_color', 'label': 'Hair Color', 'placeholder': 'Select Hair Color', 'options': ['Brown', 'Ginger', 'Blonde', 'Black', 'White', 'Purple']},
-        {'id': 'hair_type', 'label': 'Hair Type', 'placeholder': 'Select Hair Type', 'options': ['Curly', 'Straight', 'Messy', 'Wavy']},
-        {'id': 'hair_length', 'label': 'Hair Length', 'placeholder': 'Select Hair Length', 'options': ['Very Short', 'Short', 'Medium', 'Long', 'Very Long']},
-        {'id': 'skin_color', 'label': 'Skin Color', 'placeholder': 'Select Skin Color', 'options': ['White', 'Pale', 'Tan', 'Dark', 'Very Dark']},
-        {'id': 'skin_type', 'label': 'Skin Type', 'placeholder': 'Select Skin Type', 'options': ['Acne', 'Clear', 'Freckles', 'See-Through']},
-        {'id': 'age', 'label': 'Age', 'placeholder': 'Select Age', 'options': ['Child', 'Teenager', 'Adult', 'Elderly']},
-        {'id': 'ethnicity', 'label': 'Ethnicity', 'placeholder': 'Select Ethnicity', 'options': ['African', 'Caucasian', 'Italian', 'Jewish', 'British', 'Finnish', 'Mexican', 'Chinese', 'Vietnamese']},
-        {'id': 'eye_color', 'label': 'Eye Color', 'placeholder': 'Select Eye Color', 'options': ['Brown', 'Blue', 'Gray', 'Yellow', 'Green', 'Red']},
-        {'id': 'body', 'label': 'Body Type', 'placeholder': 'Select Body Type','options': ['Morbidly Obese', 'Obese', 'Chubby', 'Muscular', 'Athletic', 'Unnaturally Muscular', 'Normal', 'Thin', 'Skeleton']}
+        {'id': 'gender', 'label': 'Gender', 'label': 'Select Gender', 'options': ['Male', 'Female', 'Ugly']},
+        {'id': 'hair_color', 'label': 'Hair Color', 'label': 'Select Hair Color', 'options': ['Brown', 'Ginger', 'Blonde', 'Black', 'White', 'Purple']},
+        {'id': 'hair_type', 'label': 'Hair Type', 'label': 'Select Hair Type', 'options': ['Curly', 'Straight', 'Messy', 'Wavy']},
+        {'id': 'hair_length', 'label': 'Hair Length', 'label': 'Select Hair Length', 'options': ['Very Short', 'Short', 'Medium', 'Long', 'Very Long']},
+        {'id': 'skin_color', 'label': 'Skin Color', 'label': 'Select Skin Color', 'options': ['White', 'Pale', 'Tan', 'Dark', 'Very Dark']},
+        {'id': 'skin_type', 'label': 'Skin Type', 'label': 'Select Skin Type', 'options': ['Acne', 'Clear', 'Freckles', 'See-Through']},
+        {'id': 'age', 'label': 'Age', 'label': 'Select Age', 'options': ['Child', 'Teenager', 'Adult', 'Elderly']},
+        {'id': 'ethnicity', 'label': 'Ethnicity', 'label': 'Select Ethnicity', 'options': ['African', 'Caucasian', 'Italian', 'Jewish', 'British', 'Finnish', 'Mexican', 'Chinese', 'Vietnamese']},
+        {'id': 'eye_color', 'label': 'Eye Color', 'label': 'Select Eye Color', 'options': ['Brown', 'Blue', 'Gray', 'Yellow', 'Green', 'Red']},
+        {'id': 'body', 'label': 'Body Type', 'label': 'Select Body Type','options': ['Morbidly Obese', 'Obese', 'Chubby', 'Muscular', 'Athletic', 'Unnaturally Muscular', 'Normal', 'Thin', 'Skeleton']}
     ]
     return render(request, "create_face.html", {'guest': request.GET.get("guest"), 'form_fields': form_fields})
 
