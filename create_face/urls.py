bb454ace7fe77feba21061ec22ae9ad089b4d69e
Silas Nevstad <nevstads@gmail.com>
2024-07-02 14:58:09 -0400

    Randomizing attributes (not pretty)

--- create_face/urls.py
+++ create_face/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="create"),
+    path("randomize_attributes/", views.randomize_attributes, name="randomize_attributes"),
     path("save_image/", views.save_image, name="save_image"),
     path("delete/<int:id>/", views.delete_image, name="delete_image"),
 ]
