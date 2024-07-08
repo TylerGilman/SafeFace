e9e27c7f8d2b87db1b5b3b58c2e017c85849cba0
Tyler Gilman <tylersgilman@gmail.com>
2024-07-08 15:28:39 -0400

    fix

--- safe_face_web/settings.py
+++ safe_face_web/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django_htmx.middleware.HtmxMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 
 ]
